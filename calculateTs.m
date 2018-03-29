e933a620131faf52f3ba802d0963245e5ac9cb78
haavasl <35950701+haavasl@users.noreply.github.com>
2018-03-29 15:33:10 +0800

    Add files via upload

---
+++ calculateTs.m
@@ -0,0 +1,16 @@
+function ts = calculateTs(path, avg_v)
+ts = zeros(1,size(path,1));
+dist = path(2:size(path,1),:)-path(1:size(path,1)-1,:);
+dist2 = sqrt(dist(:,1).^2 +dist(:,2).^2 +dist(:,3).^2 );
+for i  = 2:size(path,1)
+    ts(i) = dist2(i-1)/avg_v + ts(i-1);
+    if i == 2
+        ts(i) = ts(i) + 3;
+    elseif i == size(path,1)
+        ts(i) = ts(i) + 100;
+    end
+end
+ts = round(ts,1);
+end
+
+

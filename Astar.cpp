2fb116c7aa22d4e4f98c470c93591a64ca60272e
camillakjolstad <37829013+camillakjolstad@users.noreply.github.com>
2018-04-03 11:14:38 +0800

    Update Astar.cpp
    The code is now commented

--- Astar.cpp
+++ Astar.cpp
@@ -12,6 +12,8 @@
 using std::vector;
 using namespace std;
 
+// Creating a node with information on x-, y- and z-coordinates,
+// g-, h- and f-cost, if it is an obstacle and the parent of the node
 struct Node {
 	double x;
 	double y;
@@ -46,6 +48,7 @@ struct Node {
 	}
 };
 
+// Calculate the gScore as distance from the start point
 int gScore(Node current, Node start) {
 	int dx = 2 * fabs(start.x - current.x);
 	int dy = 2 * fabs(start.y - current.y);
@@ -53,6 +56,7 @@ int gScore(Node current, Node start) {
 	return g;
 }
 
+// Calculate the hScore as distance from end point
 int hScore(Node current, Node end) {
 	int dx = 2 * fabs(end.x - current.x);
 	int dy = 2 * fabs(end.y - current.y);
@@ -61,6 +65,8 @@ int hScore(Node current, Node end) {
 
 };
 
+
+// Create grid of obstacles
 void createGrid(Node (&grid)[11][11], Node End, Node Start) {
 	int rows = 11;
 	int cols = 11;
@@ -106,18 +112,25 @@ void createGrid(Node (&grid)[11][11], Node End, Node Start) {
 	
 }
 
+// A* search algorithm
 void Astar(Node (&grid)[11][11], Node Start, Node End) {
 	int count = 0;
 	int rows = 11;
 	int cols = 11;
+	
+	// A list of discovered nodes that are not evaluated
 	list<Node> openList{};
+	// A list of already evaluated nodes
 	list<Node> closedList{};
+	// g-,h- and f-score of the start point
 	Start.g = 0;
 	Start.h = hScore(Start, End);
 	Start.f = Start.g + Start.h;
 	openList.push_back(Start);
 	Node current;
 	int score;
+	
+	// Runs until the end position is reached
 	while (!(current == End)) {
 		score = 1000;
 		for (list<Node>::iterator it = openList.begin(); it != openList.end(); ++it) {
@@ -150,6 +163,7 @@ void Astar(Node (&grid)[11][11], Node Start, Node End) {
 
 				ofstream pathFile;
 				pathFile.open("coordinates.txt", ofstream::app);
+			// Writes to the file coordinates.txt
 			if (pathFile.is_open()) {
 				for (int i = path_x.size()-1; i > -1; i--) {
 					pathFile << path_x[i] << " " << path_y[i] << endl;
@@ -163,7 +177,8 @@ void Astar(Node (&grid)[11][11], Node Start, Node End) {
 		closedList.push_back(current);
 		current.g = gScore(current, Start);
 		int tent_g = 0;
-
+		
+		// Creates a vector of neighbours that are not obstacles
 		vector<Node>* neighbours = new vector<Node>(0);
 		int i = 2 * (2.5 - current.x);
 		int j = 2 * (2.5 - current.y);
@@ -194,20 +209,21 @@ void Astar(Node (&grid)[11][11], Node Start, Node End) {
 					neighbours->push_back(grid[i + 1][j + 1]);
 				}
 		for (int i = 0; i < neighbours->size(); i++) {
+			// Checks if the neighbour is in the closedList: if the node is already evaluated
 			list<Node>::iterator findIt = find(closedList.begin(), closedList.end(), (*neighbours)[i]);
 			if (findIt == closedList.end()) {
-
 				
-				
-					
+					// The distance from start to a neigbour
 					tent_g = current.g + gScore((*neighbours)[i], current);
 					int x = int(2 * (2.5 - (*neighbours)[i].x));
 					int y = int(2 * (2.5 - (*neighbours)[i].y));
 					if (tent_g < ((*neighbours)[i].g)) {
+						// This is a better path
 						grid[x][y].parent = &grid[int(2 * (2.5 - (current.x)))][int(2 * (2.5 - (current.y)))];
 						grid[x][y].g = tent_g;
 						grid[x][y].f = (grid[x][y].g + hScore((*neighbours)[i], End));
 					}
+				// Checks if the node is in openList: if the node has been discovered but not evaluated
 				list<Node>::iterator findIt = find(openList.begin(), openList.end(), (*neighbours)[i]);
 				if (findIt == openList.end()) {
 					openList.push_back(grid[x][y]);
@@ -219,7 +235,8 @@ void Astar(Node (&grid)[11][11], Node Start, Node End) {
 	}
 }
 int main() {
-
+	
+	// Initilizing the grid
 	Node grid[11][11];
 	Node Beginning(-1.5, 1.5);
 	Node End(2, 0);
@@ -230,12 +247,17 @@ int main() {
 		}
 		cout << "\n" << "\n";
 	}
+	// Using A* search algorithm from start point to the midway point
 	Node Start = grid[8][2];
 	Node MidPoint = grid[1][5];
 	Node Goal = grid[8][8];
+	
+	// Delete previous points in file
 	ofstream pathFinder("coordinates.txt");
 	pathFinder.close();
 	Astar(grid, Start, MidPoint);
+	
+	// Using A* search algorithm from midway point to the goal point
 	Beginning = Node(2, 0);
 	End = Node(-1.5, -1.5);
 	createGrid(grid, End, Beginning);

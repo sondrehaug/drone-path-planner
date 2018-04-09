e2fc753856447862704e54fe55e2eb6074f91a85
sollimann <kristrso@stud.ntnu.no>
2018-04-09 20:53:27 +0800

    Added necessary descriptive comments for variables and constants. Also added header

--- time_planning.m
+++ time_planning.m
@@ -1,23 +1,57 @@
+%***************************************************
+% Purpose: Calculate and return a set of time
+% intervals between each point in all subpaths
+%***************************************************
+% Variables: 
+% name         I/O   Description
+% --------------------------------------------------
+% path          I    3D file with coordinates (x,y,z)
+% avg_v         I    Average velocity
+% mode          I    Flying mode of the path
+% ts            O    Z dimension to be added
+% dist1         I    2D file with coordinates (x,y)
+% dist2         O    3D file with coordinates (x,y,z)
+% count         I    Counter
+%
+% Constants:
+% name         I/O   Description
+% --------------------------------------------------
+% dt_start      I    Used for smoothening trajectory
+% dt_stop       I    Used for smoothening trajectory
+%***************************************************
+
 function ts = time_planning(path, avg_v,mode)
 
 % mode = 0 (take off), mode = 1 (middair), mode = 2 (landing)
 
-
-a = 0;
+% Initialization
+count = 0;
+dt_start = 0;
+dt_stop = 0;
 ts = zeros(1,size(path,1));
+
+% Position coordinates in 3-dimensions
 dist = path(2:size(path,1),:)-path(1:size(path,1)-1,:);
+
+% 3D-vector between points
 dist2 = sqrt(dist(:,1).^2 +dist(:,2).^2 +dist(:,3).^2 );
 
+
+% Take off
 if (mode == 0)
     dt_start = 0;
     dt_stop = 0;
+% Middair
 elseif(mode == 1)
     dt_start = 2.5;
     dt_stop = 0;
+% Landing
 else
-    dt_start = 0;
-    dt_stop = 0;
+    
+
 end
+        
+        % Calculates time interval for each subpath
         for i  = 2:size(path,1)
             ts(i) = dist2(i-1)/avg_v + ts(i-1);
             
@@ -28,22 +62,28 @@ end
             end
             
             % Check if last element in path is point B
-            % Meaning we want to stop in point B
-            if(path(size(path,1),1)== -1.500 && path(size(path,1),2) == -1.500 && path(1,1)== -1.500 && path(1,2) == 1.500)        
-                    
+            % Make sure that the velocities in subpath before and after
+            % point B has an average speed of 0.3-0.4 [m/s]
+            if(path(size(path,1),1)== -1.500 && path(size(path,1),2)...
+                    == -1.500 && path(1,1)== -1.500 && path(1,2) == 1.500)        
+                
+                % If current coordinate is point B (x,y) = (2,0)
                 if (path(i,1) == 2 && path(i,2) == 0)
                 
+                % Average speed for subpath before point B
                 ts(i) = dist2(i-1)/0.40 + ts(i-1);
-                a = a+1;
-                elseif (a == 1) 
-        
+                count = count+1;
+                elseif (count == 1) 
+                
+                % Average speed for subpath after point B
                 ts(i) = dist2(i-1)/0.40 + ts(i-1);
-                a = a+1;
+                count = count+1;
                 end %if
 
             end %if
             
         end %for
+        
         ts = round(ts,1);
 end %func
     
